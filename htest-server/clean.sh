cf0e0ca77998df097fd9cd2460e79c72d9a07306
nicolas <nicolas@14d14257-7f1e-0410-bdca-c15ec6d08fb0>
2017-02-06 18:39:33 +0000

    htest-server : Fix wildcard bug in clean.sh script
    git-svn-id: svn+ssh://substantielsvn.dyndns.org/ordissimo/sub/ordissimo/htest-server@32170 14d14257-7f1e-0410-bdca-c15ec6d08fb0

--- htest-server/clean.sh
+++ htest-server/clean.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "Cleaning build files..."
-rm -rfv "`readlink -f "$SCRIPT_DIR/.."`/htest-server_*"
+rm -rfv "`readlink -f "$SCRIPT_DIR/.."`/htest-server_"*
 rm -rfv "$SCRIPT_DIR/debian/debhelper-build-stamp"
 rm -rfv "$SCRIPT_DIR/debian/htest-server.debhelper.log"
 rm -rfv "$SCRIPT_DIR/debian/htest-server/"
