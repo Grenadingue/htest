bceeb593bc908d15767fe9e79de514e54985177f
nicolas <nicolas@14d14257-7f1e-0410-bdca-c15ec6d08fb0>
2017-03-01 18:38:37 +0000

    htest-server : Add and use hostname binding option in configuration file
    git-svn-id: svn+ssh://substantielsvn.dyndns.org/ordissimo/sub/ordissimo/htest-server@32484 14d14257-7f1e-0410-bdca-c15ec6d08fb0

--- htest-server/source/index.js
+++ htest-server/source/index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-const config = require('./config/base.config');
+const config = require('./config/base.config').webServer;
 const app = require('./libraries/express')();
 const server = require('http').createServer(app);
 const io = require('./libraries/socket.io')(server);
 const router = require('./routes');
 
-console.log(`Initializing http server on port ${config.webServer.port}...`);
+console.log(`Initializing http server to listen on ${config.hostname}:${config.port}`);
 router.init(app, io);
-server.listen(config.webServer.port, () => {
+server.listen(config.port, config.hostname, () => {
   console.log('Http server successfuly initialized\n');
 });
