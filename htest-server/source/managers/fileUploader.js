77c6eee0b2430af73fdd7e3ff1f4625d04d939f4
nicolas <nicolas@14d14257-7f1e-0410-bdca-c15ec6d08fb0>
2017-03-06 20:01:07 +0000

    htest-server : Make fileUploader manager save user's files upload history
    git-svn-id: svn+ssh://substantielsvn.dyndns.org/ordissimo/sub/ordissimo/htest-server@32558 14d14257-7f1e-0410-bdca-c15ec6d08fb0

--- htest-server/source/managers/fileUploader.js
+++ htest-server/source/managers/fileUploader.js
@@ -1,7 +1,19 @@
+const uploadedFiles = {};
+
 module.exports.onSaved = (event) => {
+  const clientId = event.file.meta.clientId;
+
+  uploadedFiles[clientId] = (uploadedFiles[clientId] === undefined ? [] : uploadedFiles[clientId]);
+  uploadedFiles[clientId].push(event.file);
+
   console.log('new file with id', event.file.id, '; received from client id', event.file.meta.clientId, ';', event.file.name, ';', event.file.pathName);
 };
 
 module.exports.onError = (event) => {
   console.log('Error from uploader', event);
+};
+
+module.exports.getLastUploadFromClientId = (clientId) => {
+  const lastUpload = uploadedFiles[clientId];
+  return lastUpload === undefined ? lastUpload : lastUpload[lastUpload.length - 1];
 };
