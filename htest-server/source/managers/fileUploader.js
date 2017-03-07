b00480d92a110ebe3a779096bbe2811a2177efe2
nicolas <nicolas@14d14257-7f1e-0410-bdca-c15ec6d08fb0>
2017-03-07 18:32:17 +0000

    htest-server : Add clients uploaded files automatic cleaner
    git-svn-id: svn+ssh://substantielsvn.dyndns.org/ordissimo/sub/ordissimo/htest-server@32601 14d14257-7f1e-0410-bdca-c15ec6d08fb0

--- htest-server/source/managers/fileUploader.js
+++ htest-server/source/managers/fileUploader.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const uploadedFiles = {};
 
 module.exports.onSaved = (event) => {
@@ -10,10 +12,21 @@ module.exports.onSaved = (event) => {
 };
 
 module.exports.onError = (event) => {
-  console.log('Error from uploader', event);
+  console.log('File upload error:', event);
 };
 
 module.exports.getLastUploadFromClientId = (clientId) => {
   const lastUpload = uploadedFiles[clientId];
   return lastUpload === undefined ? lastUpload : lastUpload[lastUpload.length - 1];
+};
+
+module.exports.clearClientUploads = (clientId) => {
+  if (uploadedFiles[clientId] !== undefined) {
+    uploadedFiles[clientId].forEach((uploadedFile) => {
+      fs.unlink(uploadedFile.pathName, () => {
+        console.log(`Removed '${uploadedFile.pathName}' associated with client id '${clientId}'`);
+      });
+    });
+  }
+  uploadedFiles[clientId] = undefined;
 };
