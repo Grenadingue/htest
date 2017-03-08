23bf9425f9e30f9905ceac11e1fbe5f6e9c811b7
nicolas <nicolas@14d14257-7f1e-0410-bdca-c15ec6d08fb0>
2017-03-08 11:27:13 +0000

    htest-server : Prevent building package with development configuration files + Detail verbose output
    git-svn-id: svn+ssh://substantielsvn.dyndns.org/ordissimo/sub/ordissimo/htest-server@32612 14d14257-7f1e-0410-bdca-c15ec6d08fb0

--- htest-server/build.bash
+++ htest-server/build.bash
@@ -2,16 +2,21 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SOURCE_DIR="$SCRIPT_DIR/source"
-DEBIAN_DIR="$SCRIPT_DIR/debian"
+CONFIG_DIR="$SOURCE_DIR/config"
+CONFIG_DIR_SAVE="$SCRIPT_DIR/config.save"
 NODE_MODULES="$SOURCE_DIR/node_modules"
 NODE_MODULES_SAVE="$SCRIPT_DIR/node_modules.save"
 
+echo "Moving current configuration '$CONFIG_DIR/*.json' inside '$CONFIG_DIR_SAVE'..."
+mkdir -v "$CONFIG_DIR_SAVE"
+mv -v "$CONFIG_DIR"/*.json "$CONFIG_DIR_SAVE"
+
 if [ -d "$NODE_MODULES" ]; then
-  echo "Saving current 'source/node_modules' as 'nodes_modules.save'..."
+  echo "Saving current '$NODE_MODULES' as '$NODE_MODULES_SAVE'..."
   mv "$NODE_MODULES" "$NODE_MODULES_SAVE"
 fi
 
-echo "Retrieving npm's production dependencies as 'source/node_modules'..."
+echo "Retrieving npm's production dependencies as '$NODE_MODULES'..."
 cd "$SOURCE_DIR"
 npm install --prod
 cd -
@@ -20,10 +25,14 @@ echo "Building debian package from package source..."
 # do not check build dependencies because archlinux's dpkg tools are unable to check it
 dpkg-buildpackage -d # '-d' means '--no-check-builddeps'
 
-echo "Deleting newly generated 'source/node_modules'..."
+echo "Deleting newly generated '$NODE_MODULES'..."
 rm -rf "$NODE_MODULES"
 
 if [ -d "$NODE_MODULES_SAVE" ]; then
-  echo "Replacing back 'node_modules.save' to 'source/node_modules'..."
+  echo "Replacing back '$NODE_MODULES_SAVE' to '$NODE_MODULES'..."
   mv "$NODE_MODULES_SAVE" "$NODE_MODULES"
 fi
+
+echo "Moving back configuration inside '$CONFIG_DIR'..."
+mv -v "$CONFIG_DIR_SAVE"/*.json "$CONFIG_DIR"
+rmdir -v "$CONFIG_DIR_SAVE"
